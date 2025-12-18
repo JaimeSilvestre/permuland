f22d6106d5227353a3d3d5daa2aa6aa9e37217be
Jaime <jaimeautor@gmail.com>
2025-12-18 22:26:36 +0100

    fix: Deployment config fix (remove invalid server key) & verify dependencies

--- next.config.ts
+++ next.config.ts
@@ -6,10 +6,7 @@ const nextConfig: NextConfig = {
       bodySizeLimit: '10mb',
     },
   },
-  // @ts-expect-error - legacy/experimental property might miss type definition
-  server: {
-    allowedDevOrigins: ["localhost:3000", "192.168.1.43:3000"],
-  },
+
   images: {
     remotePatterns: [
       {
